56f3bb6a095873896cca9cc8430ab6b21745c36b
lukat33 <lukatavcer@yahoo.com>
2020-03-18 22:44:36 +0100

    Init

--- smrpo_backend_project/urls.py
+++ smrpo_backend_project/urls.py
@@ -1,4 +1,4 @@
-"""smrpo_backend URL Configuration
+"""smrpo_backend_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
