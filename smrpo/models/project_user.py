6eef98823eed59290626776fce2ff0c37d755fd9
lukat33 <lukatavcer@yahoo.com>
2020-03-24 17:12:36 +0100

    Project user prevent null

--- smrpo/models/project_user.py
+++ smrpo/models/project_user.py
@@ -3,23 +3,26 @@ from django.db import models
 
 
 class ProjectUserRole(models.Model):
-    title = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.title
+        return "{}".format(self.title)
 
 
 class ProjectUser(models.Model):
     """
         Intermediate table for many to many field connecting projects with users.
     """
-    role = models.ForeignKey(ProjectUserRole, on_delete=models.DO_NOTHING)
+    role = models.ForeignKey(ProjectUserRole, on_delete=models.PROTECT)
 
     project = models.ForeignKey('Project', on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.user.get_full_name(), self.role)
 
     @property
     def api_data(self):
