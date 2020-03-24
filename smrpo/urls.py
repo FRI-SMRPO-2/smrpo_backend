ce5797518a00d9d24d2d707be187440cedc02925
lukat33 <lukatavcer@yahoo.com>
2020-03-24 17:43:55 +0100

    Users api

--- smrpo/urls.py
+++ smrpo/urls.py
@@ -3,10 +3,16 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from smrpo.views.home import HomeView
 from smrpo.views.project import ProjectView, ProjectsView
+from smrpo.views.user import UsersView
 
 urlpatterns = [
     path('auth', obtain_auth_token, name='api_token_auth'),
     path('', HomeView.as_view(), name="home"),
+
+    # User
+    path('user/', UsersView.as_view(), name="users"),
+
+    # Project
     path('project/', ProjectsView.as_view(), name="projects"),
     path('project/<int:pk>/', ProjectView.as_view(), name="project"),
 ]
