5b5ee1dd800fc9fdc902c1ef8323ce65a7a9d22e
lukat33 <lukatavcer@yahoo.com>
2020-03-26 15:58:53 +0100

    Settings

--- smrpo_project/common_settings.py
+++ smrpo_project/common_settings.py
@@ -203,8 +203,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
