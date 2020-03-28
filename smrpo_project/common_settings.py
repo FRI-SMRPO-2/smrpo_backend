52063acd129a3f5ebde1b050065df20aea783ae0
Luka Tavčer <lukatavcer@yahoo.com>
2020-03-28 14:59:38 +0100

    CORS

--- smrpo_project/common_settings.py
+++ smrpo_project/common_settings.py
@@ -221,8 +221,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
 }
 
-CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:8080',
-    'http://127.0.0.1',
-]
+# TODO remove this later!
+CORS_ORIGIN_ALLOW_ALL = True
