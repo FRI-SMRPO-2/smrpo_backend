7bfb41fd1af2c559f95b6ed2dfdffb75de5b433a
Aleks Vujic <aleks.vujic@gmail.com>
2020-04-18 14:34:01 +0200

    Get project if user is product owner

--- smrpo/views/realize_stories.py
+++ smrpo/views/realize_stories.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse, HttpResponse
 from django.utils import timezone
 from rest_framework.views import APIView
 
+from smrpo.models.project import Project
+
 
 class RealizeStoriesView(APIView):
     """
@@ -11,4 +13,12 @@ class RealizeStoriesView(APIView):
     def put(self, request, project_id):
         user = request.user
 
-        return HttpResponse("OK", status=200)
+        if user.is_superuser:
+            project = Project.objects.get(pk=project_id)
+        else:
+            try:
+                project = Project.objects.filter(product_owner=user).distinct().get(pk=project_id)
+            except Project.DoesNotExist:
+                return HttpResponse("Projekt ne obstaja ali pa uporabnik ni product owner", status=404)
+
+        return JsonResponse(project.api_data, status=200)
