3941ce452a537c35daa4ab5e2eeae2c1b7816deb
Aleks Vujic <aleks.vujic@gmail.com>
2020-05-04 15:23:27 +0200

    Superuser can access current sprint

--- smrpo/views/current_sprint.py
+++ smrpo/views/current_sprint.py
@@ -17,10 +17,14 @@ class ActiveSprintView(APIView):
             # get current sprint
             now = timezone.now()
 
-            sprint = Sprint.objects.filter(project_id=project_id).filter(
-                Q(project__scrum_master=user) | Q(project__product_owner=user) | Q(
-                    project__developers=user)).distinct().get(
-                start_date__lte=now, end_date__gte=now)
+            if user.is_superuser:
+                sprint = Sprint.objects.filter(project_id=project_id).distinct().get(start_date__lte=now,
+                                                                                     end_date__gte=now)
+            else:
+                sprint = Sprint.objects.filter(project_id=project_id).filter(
+                    Q(project__scrum_master=user) | Q(project__product_owner=user) | Q(
+                        project__developers=user)).distinct().get(
+                    start_date__lte=now, end_date__gte=now)
             return JsonResponse(sprint.api_data, safe=False)
         except Sprint.DoesNotExist:
             return HttpResponse('Trenutno ni aktiven noben sprint!', status=404)
