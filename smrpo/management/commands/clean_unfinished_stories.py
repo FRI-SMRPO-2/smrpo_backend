ed0fe3af2aea4af0869e037680269371c8161e09
Luka Tavčer <lukatavcer@yahoo.com>
2020-05-04 16:49:53 +0200

    Auto rejected comment

--- smrpo/management/commands/clean_unfinished_stories.py
+++ smrpo/management/commands/clean_unfinished_stories.py
@@ -8,5 +8,8 @@ class Command(BaseCommand):
     def handle(self, **options):
         now = timezone.now()
         unfinished_stories = Story.objects.filter(sprint__isnull=False, sprint__end_date__lt=now, realized=False)
-        updated = unfinished_stories.update(sprint=None)
+        updated = unfinished_stories.update(
+            sprint=None,
+            rejection_comment="Zgodba ni bila realizirana do konca aktivnega sprinta."
+        )
         print(f"Cleaned {updated} unfinished stories.")
