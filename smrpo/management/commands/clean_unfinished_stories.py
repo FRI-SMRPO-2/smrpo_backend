f1d69abf849167fe5410a4d72fb11191827964cc
Luka Tavčer <lukatavcer@yahoo.com>
2020-05-07 15:18:16 +0200

    Clear assignee awaaitings

--- smrpo/management/commands/clean_unfinished_stories.py
+++ smrpo/management/commands/clean_unfinished_stories.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from smrpo.models.story import Story
+from smrpo.models.task import Task
 
 
 class Command(BaseCommand):
@@ -12,4 +13,14 @@ class Command(BaseCommand):
             sprint=None,
             rejection_comment="Zgodba ni bila realizirana do konca aktivnega sprinta."
         )
+
         print(f"Cleaned {updated} unfinished stories.")
+
+        # Only temporary
+        tasks = Task.objects.filter(story__sprint__end_date__lt=now, assignee_awaiting__isnull=False)
+        updated = tasks.update(assignee_awaiting=None)
+        print(f"Cleaned {updated} unfinished tasks.")
+
+        for story in unfinished_stories:
+            story.tasks.filter(assignee_awaiting__isnull=False).update(assignee_awaiting=None)
+
