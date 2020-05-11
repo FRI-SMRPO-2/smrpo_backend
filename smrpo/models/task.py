2bb5eda494aa8e873ed8ca018f736437e8152abe
Luka Tavčer <lukatavcer@yahoo.com>
2020-05-11 15:55:13 +0200

    Error fix

--- smrpo/models/task.py
+++ smrpo/models/task.py
@@ -79,7 +79,7 @@ class Task(models.Model):
             return "Nalogo lahko zavrne le uporabnik, kateremu je bila dodeljena ({}).".format(self.assignee_awaiting.username)
 
         if self.assignee and self.assignee == user:
-            return "Nalogo lahko zavrne le uporabnik, kateremu je bila dodeljena ({}).".format(self.assignee_awaiting.username)
+            return "Nalogo lahko zavrne le uporabnik, kateremu je bila dodeljena ({}).".format(self.assignee.username)
 
         self.assignee_awaiting = None
         self.assignee = None
