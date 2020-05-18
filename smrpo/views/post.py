50be87219924fb76f863248df7b3e25cae67ebec
Aleks Vujic <aleks.vujic@gmail.com>
2020-05-18 21:23:06 +0200

    Adding comments

--- smrpo/views/post.py
+++ smrpo/views/post.py
@@ -1,5 +1,9 @@
 from django.http import JsonResponse, HttpResponse
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+
+from smrpo.models.post import Post
+from smrpo.models.project import Project
 
 
 class PostView(APIView):
@@ -7,4 +11,30 @@ class PostView(APIView):
         Add post to project
     """
     def post(self, request, project_id):
-        return JsonResponse("OK", safe=False, status=200)
+        user = request.user
+        data = request.data
+
+        # get project
+        project = get_object_or_404(Project, pk=project_id)
+
+        # check if user is part of the project
+        if not user.is_superuser:
+            is_developer = project.developers.all().filter(pk=user.api_data()['id']).exists()
+            if not (project.product_owner == user or project.scrum_master == user or is_developer):
+                return HttpResponse('User is forbidden to access this resource.', status=403)
+
+        text = data.get('text')
+
+        if not text:
+            return HttpResponse("Besedilo ni podano", status=400)
+
+        try:
+            post = Post()
+            post.text = text
+            post.created_by = user
+            post.project = project
+            post.save()
+        except:
+            return HttpResponse("Napaka pri dodajanju objave", status=500)
+
+        return JsonResponse(project.api_data, safe=False, status=201)
